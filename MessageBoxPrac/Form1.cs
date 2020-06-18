8cb3d8109ca9d39f639890a50ed75ee6c1192b49
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-06-18 14:11:36 +0900

    MessageBox 실습

--- MessageBoxPrac/Form1.cs
+++ MessageBoxPrac/Form1.cs
@@ -16,5 +16,21 @@ namespace MessageBoxPrac
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //입력 받은 값에 따라서 사용자의 입력을 기다린다.
+            MessageBox.Show("내용");
+            MessageBox.Show("내용", "제목");
+            MessageBox.Show("내용", "제목", MessageBoxButtons.AbortRetryIgnore);
+
+            DialogResult result;
+            do
+            {
+                result = MessageBox.Show("내용", "제목", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Information);
+
+            } while (result == DialogResult.Retry);
+
+        }
     }
 }
